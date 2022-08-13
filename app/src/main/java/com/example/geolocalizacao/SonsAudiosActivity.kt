267c183c77469c9b59ac50f4a57d707331d99e2d
Danilo Santos <danilopsnts@gmail.com>
2022-08-12 22:37:02 -0300

    remove alertas

--- app/src/main/java/com/example/geolocalizacao/SonsAudiosActivity.kt
+++ app/src/main/java/com/example/geolocalizacao/SonsAudiosActivity.kt
@@ -7,15 +7,14 @@ import android.view.View
 
 class SonsAudiosActivity : AppCompatActivity() {
 
-    var cow: MediaPlayer? = null
-    var dog: MediaPlayer? = null
+    private var cow: MediaPlayer? = null
+    private var dog: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sons_audios)
-        /*
-        * Recuperando os arquivos de audio e armazenando nas variaveis
-        * */
+        // * Recuperando os arquivos de audio e armazenando nas variaveis
+        // *
         cow = MediaPlayer.create(this, R.raw.cow)
         dog = MediaPlayer.create(this, R.raw.dog)
     }
@@ -25,10 +24,11 @@ class SonsAudiosActivity : AppCompatActivity() {
     * para o dog e a cow
     * */
 
-    fun soundDog(view: View) {
+    fun soundDog(@Suppress("UNUSED_PARAMETER") view: View) {
         dog!!.start()
     }
-    fun soundCow(view: View) {
+
+    fun soundCow(@Suppress("UNUSED_PARAMETER") view: View) {
         cow!!.start()
     }
 }
